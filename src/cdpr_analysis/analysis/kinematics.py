7a66a23a847795200dbbfb3a618006d907473f92
Patrick Huynh <patrickhuynh3@gmail.com>
2024-07-17 17:19:14 +0200

    feat: refactor into proper file structure

--- src/cdpr_analysis/analysis/kinematics.py
+++ src/cdpr_analysis/analysis/kinematics.py
@@ -1,6 +1,6 @@
-from typing import List
 from cdpr_analysis.model import CableDrivenParallelRobot
 from cdpr_analysis.types import Pose
+from typing import List
 
 
 class ForwardKinematics:
@@ -10,3 +10,12 @@ class ForwardKinematics:
     def calculate(self, cableLengths: List[float]) -> Pose:
         print("calculating forward kinematics")
         return Pose([0.0, 0.0, 0.0], [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+
+
+class InverseKinematics:
+    def __init__(self, cdpr: CableDrivenParallelRobot):
+        self.cdpr = cdpr
+
+    def calculate(self, pose: Pose) -> List[float]:
+        print("calculating inverse kinematics")
+        return [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
